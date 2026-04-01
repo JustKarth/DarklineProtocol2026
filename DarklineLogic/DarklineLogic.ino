9e74f2bc419ef3aae7130ef4385666988afca4c3
Karthik <karthikkonala14@gmail.com>
2026-04-01 12:55:33 +0530

    Update DarklineLogic.ino
    Added some functions for normalization of readings, updating values and also introduced the Alan Constant.

--- DarklineLogic/DarklineLogic.ino
+++ DarklineLogic/DarklineLogic.ino
@@ -1,17 +1,18 @@
 //Sensor and calibration variables
+const int AlanConstant = 1000;//Used for scaling all the normalized fractions to a standardized range in honor of Alan
+const int normalizedThreshold = AlanConstant/2;
 const int sensorPins[8] = {A0, A1, A2, A3, A4, A5, A6, A7};//Pre-defines the pins from which the arduino reads/the ones connected to IR sensor
 const int weights[8] = {-3500, -2500, -1500, -500, 500, 1500, 2500, 3500};//Pre-assigned weights for calculating the weighted mean
 bool polarity;//0 means black tape on white bg(darker tape), 1 means white tape on black bg(lighter tape), actual colors could be anything
-int sensorRaw[8];//Direct readings, Analog
-int sensorNormalized[8];//Normalized form of direct readings
-int sensorMin[8];//Min value each sensor reads (defines limits)
-int sensorMax[8];//Max value each sensor reads (defines limits)
-int sensorThreshold[8];//Mid point of min and max to act as a limit to classify the reading as dark or light
+int sensorRaw[8] = {0, 0, 0, 0, 0, 0, 0, 0};//Direct readings, Analog
+int sensorNormalized[8] = {0, 0, 0, 0, 0, 0, 0, 0};//Normalized form of direct readings
+int sensorMin[8] = {1023, 1023, 1023, 1023, 1023, 1023, 1023, 1023};//Min value each sensor reads (defines limits)
+int sensorMax[8] = {0, 0, 0, 0, 0, 0, 0, 0};//Max value each sensor reads (defines limits)
 bool sensorBool[8];//It's 0 or 1 based on whether the bot detects a line or not
-int weightedLinePosition;//Gives the weighted number which determines where the line is
-bool lineDetected;//true if atleast one sensor sees the line
-int activeSensorCount;//How many sensors detect a line
-bool isCalibrated;
+int weightedLinePosition = 0;//Gives the weighted number which determines where the line is
+bool lineDetected = false;//true if atleast one sensor sees the line
+int activeSensorCount = 0;//How many sensors detect a line
+bool isCalibrated = false;
 
 //PID Variables
 float errorP;//current error
@@ -63,11 +64,40 @@ unsigned long turnStartTime;//stores the timestamp of when it starts turning
 unsigned long junctionStartTime;//stores the timestamp of when the junction was seen
 unsigned long buttonDebounce;//Acts as a debounce for the physical buttons
 
+void updateMinMax(){
+  for(int i = 0; i<8; i++){
+    if(sensorRaw[i]<sensorMin[i]) sensorMin[i] = sensorRaw[i];
+    if(sensorRaw[i]>sensorMax[i]) sensorMax[i] = sensorRaw[i];
+  }
+}
+
 void readSensors(){
   for(int i = 0; i<8; i++) sensorRaw[i] = analogRead(sensorPins[i]);
 }
 
-void normalizeReadings();//To be defined
+void normalizeReadings(){
+  for(int i = 0; i<8; i++){
+    if(sensorMax[i]-sensorMin[i]==0){
+      sensorNormalized[i] = 0;
+      continue;
+    }
+    sensorNormalized[i] = ((long)(sensorRaw[i]-sensorMin[i])*AlanConstant)/(sensorMax[i]-sensorMin[i]);
+    sensorNormalized[i] = constrain(sensorNormalized[i], 0, AlanConstant);
+  }
+}
+
+void updateSensorBool(){
+  activeSensorCount = 0;
+  lineDetected = false;
+  for(int i = 0; i<8; i++){
+    if(polarity==0) sensorBool[i] = sensorNormalized[i]<normalizedThreshold;
+    else sensorBool[i] = sensorNormalized[i]>normalizedThreshold;
+    if(sensorBool[i]){
+      activeSensorCount++;
+      lineDetected = true;
+    }
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
